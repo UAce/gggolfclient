790078d0b6d42ccd34e9b3c5be6736600a911315
Yu-Yueh Liu <yu-yueh@hotmail.com>
2018-04-24 22:44:20 -0400

    Add credentials file and main script

---
+++ credentials_template.py
@@ -0,0 +1,5 @@
+### This file is a template for the credentials
+### Create a new file credentials_user.py with your own credentials
+
+memberId = 'Insert Member Number'
+password = 'Insert Password'
