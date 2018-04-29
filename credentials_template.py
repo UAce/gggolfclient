11c53d4dd8c0581392629a9233ec76f65a7fa486
Yu-Yueh Liu <yu-yueh@hotmail.com>
2018-04-29 19:02:07 -0400

    Update README.md and credentials_template.py

--- credentials_template.py
+++ credentials_template.py
@@ -1,6 +1,8 @@
 ### This file is a template for the credentials
-### Create a new file credentials_user.py with your own credentials
+### Create a new file credentials_info.py with your own credentials
 
-memberId = '[Insert Member Number]'
-password = '[Insert Password]'
-base_url = 'https://secure.gggolf.ca/[Insert Location Name]/index.php?'
+memberId = "[Insert Member Number]"
+password = "[Insert Password]"
+base_url = "https://secure.gggolf.ca/[Insert Location= Name]/index.php?"
+name1 = "[Insert your first name in capital letters]"
+name2 = "[Insert another person's name in capital letters]"
